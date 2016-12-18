a5bd92a3211ca8d634c5e1628bd8da378b73ca65
Shirif <saga2002@gmail.com>
2016-12-18 16:58:52 +0300

    work

--- Arduino/myhomeGatewayW5100MQTTClient/myhomeGatewayW5100MQTTClient.ino
+++ Arduino/myhomeGatewayW5100MQTTClient/myhomeGatewayW5100MQTTClient.ino
@@ -74,15 +74,29 @@
 
 #include <Ethernet.h>
 #include <MySensors.h>
-
-
 #include <DHT.h>
-#define DHTPIN 4     // what digital pin we're connected to
 
-// Uncomment whatever type you're using!
-//#define DHTTYPE DHT11   // DHT 11
-#define DHTTYPE DHT22   // DHT 22  (AM2302), AM2321
-//#define DHTTYPE DHT21   // DHT 21 (AM2301)
+
+
+
+// Set this to the pin you connected the DHT's data pin to
+#define DHT_DATA_PIN 30
+
+// Set this offset if the sensor has a permanent small offset to the real temperatures
+#define SENSOR_TEMP_OFFSET 0
+
+// Sleep time between sensor updates (in milliseconds)
+// Must be >1000ms for DHT22 and >2000ms for DHT11
+static const uint64_t UPDATE_INTERVAL = 3000;
+
+// Force sending an update of the temperature after n sensor reads, so a controller showing the
+// timestamp of the last update doesn't show something like 3 hours in the unlikely case, that
+// the value didn't change since;
+// i.e. the sensor would force sending an update every UPDATE_INTERVAL*FORCE_UPDATE_N_READS [ms]
+static const uint8_t FORCE_UPDATE_N_READS = 10;
+
+
+
 
 
 #define CHILD_ID_HUM 0
@@ -90,25 +104,26 @@
 
 #define CHILD_ID_MOT 2
 
-#define DIGITAL_INPUT_SENSOR 2   // The digital input you attached your motion sensor.  (Only 2 and 3 generates interrupt!)
+#define CHILD_ID_LIGHT 3
 
-DHT dht(DHTPIN, DHTTYPE);
+#define DIGITAL_INPUT_SENSOR 21   // The digital input you attached your motion sensor.  (Only 2 and 3 generates interrupt!)
+
+#define LIGHT_SENSOR_ANALOG_PIN 62
+
 float lastTemp;
 float lastHum;
-boolean metric = false;
+uint8_t nNoUpdatesTemp;
+uint8_t nNoUpdatesHum;
+bool metric = true;
+
+int lastLightLevel;
+
 
 MyMessage msgHum(CHILD_ID_HUM, V_HUM);
 MyMessage msgTemp(CHILD_ID_TEMP, V_TEMP);
 MyMessage msgMot(CHILD_ID_MOT, V_TRIPPED);
-
-
-void setup() {
-
-dht.begin(); 
-
-  pinMode(DIGITAL_INPUT_SENSOR, INPUT);      // sets the motion sensor digital pin as input
-
-}
+MyMessage msgLight(CHILD_ID_LIGHT, V_LIGHT_LEVEL);
+DHT dht;
 
 void presentation() {
 
@@ -118,12 +133,101 @@ void presentation() {
   // Register all sensors to gw (they will be created as child devices)
   present(CHILD_ID_HUM, S_HUM);
   present(CHILD_ID_TEMP, S_TEMP);
-  present(CHILD_ID_MOT, V_TRIPPED);
+  present(CHILD_ID_MOT, S_MOTION);
+  present(CHILD_ID_LIGHT, S_LIGHT_LEVEL);
+
+  metric = getConfig().isMetric;
+}
+
+void setup()
+{
+  dht.setup(DHT_DATA_PIN); // set data pin of DHT sensor
+  if (UPDATE_INTERVAL <= dht.getMinimumSamplingPeriod()) {
+    Serial.println("Warning: UPDATE_INTERVAL is smaller than supported by the sensor!");
+  }
+  // Sleep for the time of the minimum sampling period to give the sensor time to power up
+  // (otherwise, timeout errors might occure for the first reading)
+  sleep(dht.getMinimumSamplingPeriod());
+
+  pinMode(DIGITAL_INPUT_SENSOR, INPUT);      // sets the motion sensor digital pin as input
+
 }
 
 
-void loop() {
-  // Send locally attech sensors data here
+
+void loop()
+{
+
+// Read digital motion value
+  boolean tripped = digitalRead(DIGITAL_INPUT_SENSOR) == HIGH; 
+        
+  Serial.println(tripped);
+  send(msgMot.set(tripped?"1":"0"));  // Send tripped value to gw 
+  
+ 
+
+
+
+// Read analog light value
+  int lightLevel = (1023-analogRead(LIGHT_SENSOR_ANALOG_PIN))/10.23; 
+  Serial.println(lightLevel);
+  if (lightLevel != lastLightLevel) {
+      send(msgLight.set(lightLevel));
+      lastLightLevel = lightLevel;
+  }
+  
+
+ // Force reading sensor, so it works also after sleep()
+  dht.readSensor(true);
+  
+ // Get temperature from DHT library
+  float temperature = dht.getTemperature();
+  if (isnan(temperature)) {
+    Serial.println("Failed reading temperature from DHT!");
+  } else if (temperature != lastTemp || nNoUpdatesTemp == FORCE_UPDATE_N_READS) {
+    // Only send temperature if it changed since the last measurement or if we didn't send an update for n times
+    lastTemp = temperature;
+    if (!metric) {
+      temperature = dht.toFahrenheit(temperature);
+    }
+    // Reset no updates counter
+    nNoUpdatesTemp = 0;
+    temperature += SENSOR_TEMP_OFFSET;
+    send(msgTemp.set(temperature, 1));
+
+    #ifdef MY_DEBUG
+    Serial.print("T: ");
+    Serial.println(temperature);
+    #endif
+  } else {
+    // Increase no update counter if the temperature stayed the same
+    nNoUpdatesTemp++;
+  }
+
+  // Get humidity from DHT library
+  float humidity = dht.getHumidity();
+  if (isnan(humidity)) {
+    Serial.println("Failed reading humidity from DHT");
+  } else if (humidity != lastHum || nNoUpdatesHum == FORCE_UPDATE_N_READS) {
+    // Only send humidity if it changed since the last measurement or if we didn't send an update for n times
+    lastHum = humidity;
+    // Reset no updates counter
+    nNoUpdatesHum = 0;
+    send(msgHum.set(humidity, 1));
+    
+    #ifdef MY_DEBUG
+    Serial.print("H: ");
+    Serial.println(humidity);
+    #endif
+  } else {
+    // Increase no update counter if the humidity stayed the same
+    nNoUpdatesHum++;
+  }
+  
+
+  // Sleep for a while
+  sleep(UPDATE_INTERVAL); 
+ 
 }
 
 
